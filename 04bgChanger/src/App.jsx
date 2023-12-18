fe2f7b9997485fd537dc606ee37cb0ad0cde306b
Niteshmaurya <104177011+Niteshmaurya@users.noreply.github.com>
2023-12-18 22:21:32 +0530

    completed till 06CurrencyApp but have some bug

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,32 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [color, setColor] = useState('olive')
 
   return (
-    
+
     <div className='w-full h-screen duration-200'
-    style={{backgroundColor: color }}>
+      style={{ backgroundColor: color }}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-4 py-2 rounded-2xl'>
-          <button 
-          onClick={()=>setColor("red")}
-          className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
-        style={{backgroundColor : "red"}}>Red</button>
+        <div className='flex flex-wrap justify-center gap-3 shadow-xlg bg-white px-4 py-2 rounded-2xl'>
           <button
-          onClick={()=>setColor("green")}
-          className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
-        style={{backgroundColor : "green"}}>Green</button>
+            onClick={() => setColor("red")}
+            className='outline-none px-4 py-1 rounded-full shadow-xlg text-white'
+            style={{ backgroundColor: "red" }}>Red</button>
+
           <button
-          onClick={()=>setColor("blue")}
-          className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
-        style={{backgroundColor : "blue"}}>Blue</button>
-          </div></div>    
+            onClick={() => setColor("green")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+            style={{ backgroundColor: "green" }}>Green</button>
+          <button
+            onClick={() => setColor("blue")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+            style={{ backgroundColor: "blue" }}>Blue</button>
+          <button
+            onClick={() => setColor("pink")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+            style={{ backgroundColor: "Pink" }}>Pink</button>
+          <button
+            onClick={() => setColor("grey")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+            style={{ backgroundColor: "grey" }}>grey</button>
+          <button
+            onClick={() => setColor("yellow")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+            style={{ backgroundColor: "yellow" }}>yellow</button>
+          <button
+            onClick={() => setColor("salmon")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+            style={{ backgroundColor: "salmon" }}>salmon</button>
+          <button
+            onClick={() => setColor("lavender")}
+            className='outline-none px-4 py-1 rounded-full shadow-lg text-white'
+            style={{ backgroundColor: "lavender" }}>lavender</button>
+        </div></div>
     </div>
-    
+
   )
 }
 
--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState,useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useCallback, useEffect,useRef } from 'react'
+
+// import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed,setnumberAllowed] = useState(false)
   const [charAllowed, setcharAllowed] = useState(false)
   const [password,setpassword] = useState("")
+
+
+  // useRef hook tudum
+  const passwordRef = useRef(null)
+
 
   const passwordGen = useCallback(()=>{
     let pass = ""
@@ -16,16 +20,31 @@ function App() {
     if(numberAllowed) str = str + "0123456789"
     if(charAllowed) str = str + "!@#$%^&*()_+=[]{}\|~`"
 
-    for(let i =0;i<=length;i++){
+    for(let i =0;i< length;i++){
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+      pass += str.charAt(char)
 
     }
 
     setpassword(pass)
-
-
   },[length,numberAllowed,charAllowed,setpassword])
+
+
+  const copyPasswordToClipBoard = useCallback(()=>{
+    let max=50;
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0,3);
+
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  
+  
+
+  // passwordGen()   this doesn't work becasue we are not controllin what to render, it is controlled by setValue,setState    so we use useEffect hook
+useEffect(() => {
+  passwordGen()
+},[length,numberAllowed,charAllowed,passwordGen])
 
   return (
     <>
@@ -38,9 +57,11 @@ function App() {
         className='outline-none w-full py-1 px-3'
         placeholder='Password'   
         readOnly
-        
-        />   
-        <button
+        ref = {passwordRef}
+        /> 
+         <button
+         onClick={copyPasswordToClipBoard }
+         
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
 
         <div className='flex text-sm gap-x-1'>
@@ -53,8 +74,26 @@ function App() {
             className='cursor-pointer'
             onChange = {(e)=> {setLength(e.target.value)}}
             /> 
-            <label>Length : {length}</label>
+                <br /><label>Length :  { length}</label>
           </div>
+          <div className='flex item-center gap-x-1'></div>
+          <input type="checkbox"
+          defaultChecked = {numberAllowed}
+          
+          id = "numberInput"
+          onChange={()=>{
+            setnumberAllowed((prev)=> !prev); 
+          }} />
+          <label htmlFor="numberInput">Numbers</label>
+
+          <div className='flex item-center gap-x-1'></div>
+          <input type="checkbox"
+          defaultChecked = {charAllowed}
+          id = "charInput"
+          onChange={()=>{
+            setcharAllowed((prev)=> !prev); 
+          }} />
+          <label htmlFor="charInput">Characters</label>
         </div>
       </div>
     </div>
